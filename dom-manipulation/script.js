c39832fd0e3bb2416839eedb9feaf4fb71c6f532
Oloke Emma <emmanueloloke4@gmail.com>
2025-03-16 20:04:04 +0100

    first committing

---
+++ dom-manipulation/script.js
@@ -0,0 +1,7 @@
+// Array to hold the quotes
+let quotes = [
+    { text: "The best way to predict the future is to create it.", category: "Inspiration" },
+    { text: "Life is 10% what happens to us and 90% how we react to it.", category: "Motivation" },
+    { text: "Success is not the key to happiness. Happiness is the key to success.", category: "Happiness" }
+  ];
+  
